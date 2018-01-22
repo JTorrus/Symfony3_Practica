b27293bbe1643495ec2307fe3d011147c2d80f35
JTorrus <jtorrusgalindo@gmail.com>
2018-01-22 20:20:51 +0100

    Match creation method implemented

--- src/AppBundle/Controller/CrearPartitController.php
+++ src/AppBundle/Controller/CrearPartitController.php
@@ -25,7 +25,7 @@ class CrearPartitController extends Controller
         $repository = $em->getRepository("AppBundle:Equip");
         $equips = $repository->findAll();
 
-        $form = $this->createFormBuilder()
+        $form = $this->createFormBuilder($partit)
             ->add('equipLocal', ChoiceType::class,array(
                 'choices' => $equips,
                 'choice_label' => 'nomEquip'
@@ -49,8 +49,21 @@ class CrearPartitController extends Controller
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $equip = $form->get('nomEquip')->getData();
-            return $this->redirectToRoute('app_main_detallsequip', array('id' => $equip->getId()));
+            /** @var Partit $partit */
+            $partit = $form->getData();
+            $partit->setIDequipLocal($partit->getEquipLocal()->getId());
+            $partit->setIDequipVisitant($partit->getEquipVisitant()->getId());
+
+            if ($partit->getIDequipLocal() == $partit->getIDequipVisitant()) {
+                echo "Els equips tenen que ser diferents";
+            } else if ($partit->getGolslocal() < 0 || $partit->getGolsvisitant() < 0) {
+                echo "Els gols no poden ser negatius";
+            } else {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($partit);
+                $em->flush();
+                return $this->redirectToRoute('app_main_inici');
+            }
         }
         return $this->render('AppBundle:CrearPartit:crear.html.twig', array(
             'form' => $form->createView()
